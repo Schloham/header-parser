f2088976ead85386bc41e6946d0e68aefa160adf
Louis Schutte <schlomaham@gmail.com>
2016-11-10 19:19:53 +0000

    finish all user stories

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  
+  var ip = req.headers['x-forwarded-for'];
+  var language = req.headers["accept-language"].split(',')[0];
+  var r = require('ua-parser').parse(req.headers['user-agent']);
+  var software = r.os.toString();
+  var data = { ipadress: ip, language: language, software: software };
+  res.json(data);
 });
 
 module.exports = router;
